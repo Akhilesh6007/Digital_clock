a4cdb04588287dd8f0149371316d764ffadaa632
Akhilesh <144203558+Akhilesh6007@users.noreply.github.com>
2024-03-15 13:25:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const hourE1 = document.getElementById("hour")
+const minuteE1 = document.getElementById("minute")
+const secondE1 = document.getElementById("seconds")
+const ampmE1 = document.getElementById("ampm")
+
+function updateClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    console.log({ h, m, s })
+
+    let ampm;
+
+    if(h > 12) h = h - 12;
+
+    if (h >= 12) ampm = "PM"
+    else ampm = "AM"
+
+    hourE1.innerText = h;
+    minuteE1.innerText = m;
+    secondE1.innerText = s;
+    ampmE1.innerHTML = ampm;
+
+    setTimeout(() => updateClock(), 100)
+}
+
+updateClock()
